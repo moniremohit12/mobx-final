d778600e2e5cc9149293dc5d139c56c4b1c97385
Monire Mohit <mohit753357@gmail.com>
2020-08-12 20:05:14 +0430

    Updates

---
+++ precache-manifest.e65ae4e2dd5d25d541901a762162da70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45459fcce85921b509a0",
+    "url": "/mobx-final/static/js/runtime~main.45459fcc.js"
+  },
+  {
+    "revision": "be596e26f2ace0c1204e",
+    "url": "/mobx-final/static/js/main.be596e26.chunk.js"
+  },
+  {
+    "revision": "11d6cf79538744d822b5",
+    "url": "/mobx-final/static/js/1.11d6cf79.chunk.js"
+  },
+  {
+    "revision": "be596e26f2ace0c1204e",
+    "url": "/mobx-final/static/css/main.17b1ace3.chunk.css"
+  },
+  {
+    "revision": "78fce2bc801f5f2a9db35439deb692ce",
+    "url": "/mobx-final/index.html"
+  }
+];
